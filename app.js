e056f4e3318b223ff0cfe016d191aea1f1e2de8f
ninjaturtletavo <70444794+ninjaturtletavo@users.noreply.github.com>
2021-04-24 09:51:38 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const fetchPosts = async () => {
     const response = await fetch(`${BASE_URL}/posts`);
     const { data } = await response.json();
     const { posts } = data;
-    console.log(posts);
+
     return posts;
   } catch (error) {
     console.error(error);
@@ -28,7 +28,7 @@ const fetchMe = async () => {
       },
     });
     const data = await response.json();
-    console.log(data);
+
     return data.data;
   } catch (error) {
     console.error(error);
@@ -37,12 +37,15 @@ const fetchMe = async () => {
 
 const fetchAndRender = async () => {
   const posts = await fetchPosts();
-  renderPosts(posts.data.posts);
+  const { data } = posts;
+  const user = await fetchMe();
+  console.log(posts);
+  console.log(data);
+  return renderPosts(posts.data, user);
 };
 
 // createPostHTML appends on #posts on html page
 const renderPosts = (posts, me) => {
-  console.log(me);
   posts.forEach((post) => {
     const postElement = createPostHTML(post, me);
     $("#posts").append(postElement);
@@ -50,8 +53,8 @@ const renderPosts = (posts, me) => {
 };
 
 const postBlogEntry = async (requestBody) => {
-  console.log(requestBody);
   const token = JSON.parse(localStorage.getItem("token"));
+
   try {
     const request = await fetch(`${BASE_URL}/posts`, {
       method: "POST",
@@ -62,8 +65,40 @@ const postBlogEntry = async (requestBody) => {
       body: JSON.stringify(requestBody),
     });
     const result = await request.json();
-    console.log(result);
+
     return result;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const editBlogEntry = async (requestBody, postId) => {
+  const token = JSON.parse(localStorage.getItem("token"));
+
+  try {
+    const request = await fetch(`${BASE_URL}/posts/${postId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(requestBody),
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const deleteBlogEntry = async (postId) => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  try {
+    const request = await fetch(`${BASE_URL}/posts/${postId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
   } catch (e) {
     console.error(e);
   }
@@ -74,8 +109,6 @@ const createPostHTML = (post, me) => {
   const { title, description, price, author, location } = post;
   const author_id = author._id;
 
-  console.log(me._id, author_id);
-
   return $(`<div class="card">
   <div class="card-body">
     ${title ? `<h3 class="card-title">${title}</h3>` : ""}
@@ -85,9 +118,17 @@ const createPostHTML = (post, me) => {
     ${location ? `<p>${location}</p>` : ""}
     ${
       me._id === author_id
-        ? `<svg class="svg-icon" viewBox="0 0 20 20">
+        ? `
+        <button class="edit-container">
+        <svg class="edit-icon" viewBox="0 0 20 20">
         <path d="M18.303,4.742l-1.454-1.455c-0.171-0.171-0.475-0.171-0.646,0l-3.061,3.064H2.019c-0.251,0-0.457,0.205-0.457,0.456v9.578c0,0.251,0.206,0.456,0.457,0.456h13.683c0.252,0,0.457-0.205,0.457-0.456V7.533l2.144-2.146C18.481,5.208,18.483,4.917,18.303,4.742 M15.258,15.929H2.476V7.263h9.754L9.695,9.792c-0.057,0.057-0.101,0.13-0.119,0.212L9.18,11.36h-3.98c-0.251,0-0.457,0.205-0.457,0.456c0,0.253,0.205,0.456,0.457,0.456h4.336c0.023,0,0.899,0.02,1.498-0.127c0.312-0.077,0.55-0.137,0.55-0.137c0.08-0.018,0.155-0.059,0.212-0.118l3.463-3.443V15.929z M11.241,11.156l-1.078,0.267l0.267-1.076l6.097-6.091l0.808,0.808L11.241,11.156z"></path>
-    </svg>`
+      </svg>
+      </button>
+      <button class="delete-container">
+      <svg class="delete-icon" viewBox="0 0 20 20">
+      <path d="M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306"></path>
+    </svg>
+    </button>`
         : ""
     }
   </div>
@@ -111,11 +152,9 @@ const registerUser = async (usernameValue, passwordValue) => {
         },
       }),
     });
-    console.log(response);
     const {
       data: { token },
     } = await response.json();
-    console.log(token);
     localStorage.setItem("token", JSON.stringify(token));
     hideRegistration();
   } catch (error) {
@@ -140,7 +179,6 @@ const loginUser = async (usernameValue, passwordValue) => {
         },
       }),
     });
-    console.log(response);
     const {
       data: { token },
     } = await response.json();
@@ -221,6 +259,29 @@ $(".form-submit").on("submit", async (e) => {
   } catch (error) {
     console.error(error);
   }
+});
+
+$("#posts").on("click", ".edit-icon", async (e) => {
+  console.log("YESSSSS edit");
+});
+
+$("#posts").on("click", ".delete-icon", async (event) => {
+  event.preventDefault();
+  console.log("YESSSSS delete");
+
+  event.preventDefault();
+  const listingElement = $(event.target).closest(".card");
+  console.log(listingElement);
+  const data = listingElement.data();
+  console.log(data);
+  const {
+    post: { _id },
+  } = data;
+  console.log(_id);
+  console.log("clicked");
+
+  deleteBlogEntry(_id);
+  fetchAndRender();
 });
 
 // MODAL section---------------------------------------------------------------
